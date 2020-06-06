26bbd4e8a8106aa6881bcf64656b83a2d4119de4
elit-altum <manan.sharma311@gmail.com>
2020-06-06 13:52:31 +0530

    add: starter files

---
+++ routes/userRouter.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+// Create your routers here and mount them in app.js
+
+const router = express.Router();
+
+module.exports = router;
---
+++ utils/errorHandler.js
@@ -0,0 +1,1 @@
+// Handle your errors here
